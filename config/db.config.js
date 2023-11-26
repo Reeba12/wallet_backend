79d99642ac702a9a7ed2f6f3b84146b3efd765ce
unza730 <unzasohail819@gmail.com>
2023-11-27 01:05:17 +0500

    change host

--- config/db.config.js
+++ config/db.config.js
@@ -2,8 +2,14 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
+console.log(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+  process.env.HOST
+);
 
-console.log(process.env.DATABASE, process.env.USER, process.env.PASSWORD)
+// consolepen.log(process.env.DATABASE, process.env.USER, process.env.PASSWORD)
 const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.env.PASSWORD, {
     host: process.env.HOST,
     dialect: 'mysql',
