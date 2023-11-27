ceb8c08796b9c4e8503dfe841321c925a7c3e542
unza730 <unzasohail819@gmail.com>
2023-11-27 13:22:30 +0500

    fix issue

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 
 import express from 'express';
-import walletRoutes from './walletRoutes'
-import authRoutes from './auth.routes'
+// import walletRoutes from './walletRoutes'
+// import authRoutes from './auth.routes'
 import bankRoutes from './bank.routes'
 import userRoutes from './user.routes.js';
 import walletRoutes from './walletRoutes.js'
